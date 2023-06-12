fcf1260ed41ebc0cffa8890cc92ba2e7016c1d42
Leonardo BENITEZ <leonardo.benitez@naranja.com>
2023-06-12 13:49:31 -0500

    config deploy firebase

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ang14-material-ui-crud'),
+      dir: require('path').join(__dirname, './coverage/frontend-manage-employees'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ang14-material-ui-crud'`, () => {
+  it(`should have as title 'frontend-manage-employees'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang14-material-ui-crud');
+    expect(app.title).toEqual('frontend-manage-employees');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ang14-material-ui-crud app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-manage-employees app is running!');
   });
 });
